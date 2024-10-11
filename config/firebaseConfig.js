baa2546ceb3170d27dbd3fb98a11cf66fab7a6c5
duykhanh <duykhanhz112233@gmail.com>
2024-10-11 18:18:06 +0700

    fix cart

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,12 +1,6 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig={
-    apiKey: 'AIzaSyDEI6fnhq1NiXzqmvrGSQ9c5PvLNY3a2vA',
-    authDomain: 'fashion-store-dfaaf',
-    projectId: 'fashion-store-dfaaf',
-    storageBucket: 'fashion-store-dfaaf.appspot.com',
-    messagingSenderId: '1064174166334',
-    appId: '1:1064174166334:android:9b99265b19981b23a14c6f',
 }
 
 export const app=initializeApp(firebaseConfig)
